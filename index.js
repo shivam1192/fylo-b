b89fdc918090154032e882daccd337ed48101a69
shivam1192 <sg9827252555@gmail.com>
2021-05-16 14:24:46 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const data = require("./data.json");
-let port = 3000;
+let port = process.env.PORT || 3000;
 app.get("/",(req,res)=>{
   res.send("hello world");
 })
