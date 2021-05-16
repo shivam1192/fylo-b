7674afd8e2823a6a29d744b6ac85a74eef7b94e2
shivam1192 <sg9827252555@gmail.com>
2021-05-16 14:20:08 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+let port = 3000;
+app.get("/",(req,res)=>{
+  res.send("hello world");
+})
+app.get("/player",(req,res)=>{
+  res.send(data);
+})
+app.listen(port,()=>{
+  console.log("Your server is running on port 3000");
+})
