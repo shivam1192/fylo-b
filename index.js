d789be480e9c6504994c9bed5c720bb4e5890afb
shivam1192 <sg9827252555@gmail.com>
2021-05-17 00:51:14 +0530

    written two endpoints

--- index.js
+++ index.js
@@ -6,18 +6,36 @@ const client = require("./db.js");
 let port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.get("/",async(req,res)=>{
+
+app.get("/api/branches/autocomplete",async(req,res)=>{
   try{
-    const q = await client.query("SELECT Name FROM banks");
+    var queryParameter = req.query;
+    console.log(queryParameter);
+    let str = "SELECT * FROM branches WHERE Branch SIMILAR TO '"+queryParameter.q+"%' ORDER BY Ifsc LIMIT "+queryParameter.limit+" OFFSET "+queryParameter.offset
+    console.log(str);
+    const q = await client.query(str);
     res.json(q.rows);
   }catch(error){
-    console.log("hello wor")
-    console.log(error);
+    res.send("No result Found");
   }
 })
-app.get("/player",(req,res)=>{
-  res.send(data);
+
+app.get("/api/branches",async(req,res)=>{
+  try{
+    var queryParameter = req.query;
+    console.log(queryParameter);
+    const para =  queryParameter.q.toUpperCase();
+    let str = "SELECT * FROM branches WHERE Ifsc = '"+para+"' OR Branch = '"+para+"' OR Address = '"+queryParameter.q+"' OR City = '"+para+"' OR District = '"+para+"' OR State = '"+para+"' ORDER BY Ifsc LIMIT "+queryParameter.limit+" OFFSET "+queryParameter.offset
+    console.log(str);
+    const q = await client.query(str);
+    res.json(q.rows);
+  }catch(error){
+    res.send("No result Found");
+  }
 })
+
 app.listen(port,()=>{
   console.log("Your server is running on port 3000");
 })
+
+//SELECT * FROM branches WHERE Ifsc = 'RTGS-HO' OR Bank_id = 60 OR Branch = 'RTGS-HO' OR Address = 'RTGS-HO' OR City = 'RTGS-HO' OR District = 'AKOLA' OR State = 'RTGS-HO' ORDER BY Ifsc LIMIT 12 OFFSET 0
